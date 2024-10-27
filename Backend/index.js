b138634b8a3d00536a2d8ca0bf4524658af014b1
Long <thdailong@gmail.com>
2024-10-27 17:15:51 +0700

    Init project backend and all to retrieve data from api using express and axios

---
+++ Backend/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const axios = require("axios");
+const PORT = 4000;
+
+// "/repos" endpoint to get repository data
+app.get("/repos", async (req, res) => {
+  try {
+    const response = await axios.get(
+      "https://api.github.com/users/freeCodeCamp/repos"
+    );
+    let repositories = response.data; // Access the data from the response
+    repositories?.filter((r) => r?.fork === false && r?.forks > 5);
+    res.json(repositories);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
